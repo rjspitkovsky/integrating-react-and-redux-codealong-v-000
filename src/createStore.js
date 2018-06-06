729aab59cd0514cd9c8e3be6a61291e968fb1eec
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-06-06 18:01:32 +0000

    automatically backed up by learn

--- src/createStore.js
+++ src/createStore.js
@@ -1,1 +1,19 @@
+function createStore(reducer) {
+  let state;
 
+  function dispatch(action) {
+    state = reducer(state, action);
+    console.log(`the state is ${state.count}`);
+    console.log(`the action is ${action.type}`);
+    // render();
+  };
+
+  function getState(){
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState
+  };
+};
