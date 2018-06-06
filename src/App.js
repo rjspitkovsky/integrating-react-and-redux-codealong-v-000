613644163444bf21c07822b87c12876aeed6dbca
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-06-06 18:00:21 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="App" >
-        Counter Component here.
+        <Counter store={this.props.store} />
       </div>
     );
   }
